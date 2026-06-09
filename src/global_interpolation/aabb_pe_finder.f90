636dfb5fb04a75737b274ee38b9ac26c49433acf
Adalberto <adalberto0524@gmail.com>
2026-06-09 08:53:40 +0200

    More error messages

--- src/global_interpolation/aabb_pe_finder.f90
+++ src/global_interpolation/aabb_pe_finder.f90
@@ -144,6 +144,7 @@ contains
     this%pe_box_num = &
          max(1, ishft(1, ceiling(log(real(this%pe_box_num, rp)) / NEKO_M_LN2)))
 
+    write(*,*), 'REDUCEDEBUG PE box num before Allreduce:', this%pe_box_num
     call MPI_Allreduce(MPI_IN_PLACE, this%pe_box_num, 1, MPI_INTEGER, &
          MPI_MIN, this%comm, ierr)
     write(*,*), 'REDUCEDEBUG PE box num after Allreduce:', this%pe_box_num
--- src/global_interpolation/cartesian_pe_finder.f90
+++ src/global_interpolation/cartesian_pe_finder.f90
@@ -185,6 +185,9 @@ contains
     end if
 
 
+    write(*,*), 'REDUCEDEBUG minmaxglobal before Allreduce:', this%min_x_global, this%max_x_global
+    write(*,*), 'REDUCEDEBUG minmaxglobal before Allreduce:', this%min_y_global, this%max_y_global 
+    write(*,*), 'REDUCEDEBUG minmaxglobal before Allreduce:', this%min_z_global, this%max_z_global
     call MPI_Allreduce(MPI_IN_PLACE, this%max_x_global, 1, MPI_REAL_PRECISION, &
          MPI_MAX, this%comm, ierr)
     call MPI_Allreduce(MPI_IN_PLACE, this%max_y_global, 1, MPI_REAL_PRECISION, &
